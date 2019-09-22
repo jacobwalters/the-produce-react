1704833b09a1ad0d4c47f3189eedd261b8deacc1
Jacob Walters <jacob@dewsign.co.uk>
2019-09-22 19:54:56 +0100

    add quick tool tip for nutritional information

--- src/js/nutritional.js
+++ src/js/nutritional.js
@@ -18,7 +18,10 @@ export default class Nutritional extends React.Component {
                         </li>
                     )}
                 </ul>
-                <h4>Nutritional value per 100 g (3.5 oz)</h4>
+                <h4>Nutritional value per 100 g (3.5 oz)
+                    <span>i</span>
+                    <div>Percentages are relative to US recommendations for adults. Source: USDA Nutrient Database</div>
+                </h4>
             </div>
         )
     }
